62512350bb29e50d58a196b826df2e4c83618062
NguyenDuyViet2602 <nguyenduyviet369@gmail.com>
2025-01-07 11:48:46 +0700

    update project

--- App1.js
+++ App1.js
@@ -1,30 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, TouchableOpacity,Button, Text, TextInput, View } from 'react-native';
 import Cat from './component/Cat';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.login}>Thông báo</Text>
-        <Cat title="Bước 1 Xác định nhu cầu khách hàng"/>
-        <Cat content="Vũ Văn Hoàng sắp đến hạn lúc 01/08/2020 9:00"/>
-        <Cat date="20/08/2020, 06:00"/>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+          <Cat
+            backgroundColor='#c5d5e5'
+            title="Bước 1 Xác định nhu cầu khách hàng"
+            text="Vũ Văn Hoàng sắp hết hạn lúc 01/08/2020 9:00" 
+            time="20/08/2020, 6:00" 
+          />
+          <Cat 
+            backgroundColor='#c5d5e5'
+            title="Bạn có khách hàng mới"
+            text="Chúc mừng bạn, bạn có khách hàng mới. Hãy mau chóng liên lạc ngay."
+            time="20/08/2020 6:00" 
+          />
+          <Cat 
+            backgroundColor='#f1f8ff'
+            title="Khách hàng được chia sẻ bị trùng"
+            text="Rất tiếc, khách hàng được chia sẻ đã tồn tại trên hệ thống. Vui lòng chia sẻ khách hàng."
+            time="20/08/2020 6:00" 
+          />
+          <Cat 
+            backgroundColor='#c5d5e5'
+            title="Khách hàng được thêm bị trùng"
+            text="Rất tiếc, khách hàng được chia sẻ đã tồn tại trên hệ thống. Vui lòng thêm khách hàng."
+            time="20/08/2020 6:00"
+          />
+          <Cat 
+            backgroundColor='#f1f8ff'
+            title="Công việc sắp đến hạn trong hôm nay"
+            text="Bạn có 17 công việc sắp hết hạn trong hôm nay."
+            time="20/08/2020 6:00" 
+          />
+          <Cat 
+            backgroundColor='#f1f8ff'
+            title="Công việc đã quá hạn"
+            text="Bạn có 17 công việc bị quá hạn. Hãy kiểm tra và lên kế hoạch hoàn thành công việc."
+            time="20/08/2020 6:00" 
+          />
+        </View>
+      );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 60,
-    paddingLeft: 30,
-    paddingRight: 30,
-  },
-  login: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    marginBottom: 50,
-  },
+    container: {
+      paddingTop: 40,
+      backgroundColor: '#eef',
+      flex: 1,
+      // justifyContent: 'center',
+      alignItems: 'center',
+    },
+    box: {
+      
+      width: 415,
+      height: 100,
+      justifyContent: 'center'
+    },
 });
--- component/Cat.js
+++ component/Cat.js
@@ -5,23 +5,31 @@ import PropTypes from 'prop-types';
 const Cat = (props) => {
   return (
     <View>
-      <Text style={styles.textLabel}>{props.title} </Text>
-      <Text style={styles.text}>{props.content}</Text>
-      <Text style={styles.date}>{props.date}</Text>
+      <Text style={styles.title}>{props.title} </Text>
+      <Text style={styles.text}>{props.text}</Text>
+      <Text style={styles.time}>{props.time}</Text>
     </View>
   );
 }
 // Định nghĩa kiểu dữ liệu cho các props
 Cat.propTypes = {
     title: PropTypes.string.isRequired, // name phải là chuỗi và là bắt buộc
-  content: PropTypes.string.isRequired,  // age phải là chuỗi và là bắt buộc
-  date: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,  // age phải là chuỗi và là bắt buộc
+    time: PropTypes.string.isRequired,
 };
 const styles = StyleSheet.create({
-  textLabel: {
-    fontSize: 15,
-    fontWeight: 'bold',
-  },
+    text: {
+        marginTop: 5,
+        fontSize: 16,
+        marginLeft: 10,
+        color: 'black',
+      },
+      time: {
+        marginTop: 10,
+        fontSize:15,
+        marginLeft: 10,
+        color: 'gray',
+      }
 
 });
 
